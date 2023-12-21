bf758aca079be3b2f900abfd665ee9f824e4d541
Oluwademilade Ala <85693382+Demiladeala@users.noreply.github.com>
2023-12-21 14:55:32 +0100

    Update page.tsx

--- app/sign-up/[[...sign-up]]/page.tsx
+++ app/sign-up/[[...sign-up]]/page.tsx
@@ -2,7 +2,7 @@ import { SignUp } from "@clerk/nextjs";
  
 export default function Page() {
   return  (
-    <div classname="my-20 w-full flex items-center justify-center">
+    <div className="my-20 w-full flex items-center justify-center">
       <SignUp />
     </div>
     )
